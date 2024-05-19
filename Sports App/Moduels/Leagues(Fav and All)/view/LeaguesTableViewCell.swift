210f81ddf63887da7f63adac01aa0b3f3c0eaab1
Mayar Saleh <miarsaleh@gmail.com>
2024-05-19 03:02:47 +0300

    working in main screen

---
+++ Sports App/Moduels/Leagues(Fav and All)/view/LeaguesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LeaguesTableViewCell.swift
+//  Sports App
+//
+//  Created by Mayar on 19/05/2024.
+//
+
+import UIKit
+
+class LeaguesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
