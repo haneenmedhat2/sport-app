8a737a801b192839d45050b837d9639e35a005a7
Mayar Saleh <miarsaleh@gmail.com>
2024-05-24 16:50:35 +0300

    working in mock test

--- Sports AppTests/Mock/MockNetworkServicw.swift
+++ Sports AppTests/Mock/MockNetworkServicw.swift
@@ -7,3 +7,48 @@
 
 import Foundation
 @testable import Sports_App
+
+class MockNetworkService {
+    
+    
+    var result = [Leagues]()
+    
+    //true = error ... false = data
+    var shouldReturnError: Bool
+    
+    init(shouldReturnError: Bool) {
+        self.shouldReturnError = shouldReturnError
+    }
+    
+    let fakeJSONObj : [String: Any] = ["users" : [
+        
+        "id":1,
+        "firstName":"Terry",
+        "lastName":"Medhurst"
+        
+        ]
+    ]
+}
+
+extension MockNetworkService{
+    enum responseWithError: Error{
+        case responseError
+    }
+    
+    func fetchDataFromJson(compiltionHandler: @escaping (UsersResult?, Error?) -> Void){
+            do{
+                var data = try JSONSerialization.data(withJSONObject: fakeJSONObj)
+                result = try JSONDecoder().decode(UsersResult.self, from: data)
+                
+            }catch{
+                print(error.localizedDescription)
+            }
+        if shouldReturnError{
+            compiltionHandler(nil, responseWithError.responseError)
+
+        }else{
+            compiltionHandler(result, nil)
+        }
+        
+    }
+}
