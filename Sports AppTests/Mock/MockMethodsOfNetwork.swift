9282ed1ca2838a9ec3579e7e0ef3dbf26f1a9ac5
Mayar Saleh <miarsaleh@gmail.com>
2024-05-25 15:27:15 +0300

    working in it

--- Sports AppTests/Mock/MockMethodsOfNetwork.swift
+++ Sports AppTests/Mock/MockMethodsOfNetwork.swift
@@ -9,13 +9,30 @@ import XCTest
 @testable import Sports_App
 
 final class MockMethodsOfNetwork: XCTestCase {
+    
+    let mockService = MockNetworkService(shouldReturnError: false)
 
+    
     override func setUpWithError() throws {
     }
-
+    
     override func tearDownWithError() throws {
     }
-
- 
-
+    
+   
+    
+    func testFetchDataFailure() {
+        mockService.fetchDataFromJson {result , error in
+            if let error = error{
+                XCTFail()
+            }else{
+                print(result)
+                XCTAssertNotNil(result)
+//                XCTAssertEqual(result?.count, 4)
+//                XCTAssertEqual(result?.first?.league_name, "UEFA Europa League")
+                
+            }
+            
+        }
+    }
 }
--- Sports AppTests/Mock/MockNetworkServicw.swift
+++ Sports AppTests/Mock/MockNetworkServicw.swift
@@ -20,6 +20,35 @@ class MockNetworkService {
         self.shouldReturnError = shouldReturnError
     }
     
+    let fakeJSONObj: [String: Any] = ["result": [
+        ["league_key": 4, "league_name": "UEFA Europa League", "country_key": 1, "country_name": "eurocups", "league_logo": "https://apiv2.allsportsapi.com/logo/logo_leagues/", "country_logo": NSNull()],
+        ["league_key": 1, "league_name": "UEFA European Championship", "country_key": 1, "country_name": "eurocups", "league_logo": NSNull(), "country_logo": NSNull()],
+        ["league_key": 683, "league_name": "UEFA Europa Conference League", "country_key": 1, "country_name": "eurocups", "league_logo": NSNull(), "country_logo": NSNull()],
+        ["league_key": 3, "league_name": "UEFA Champions League", "country_key": 1, "country_name": "eurocups", "league_logo": "https://apiv2.allsportsapi.com/logo/logo_leagues/3_uefa_champions_league.png", "country_logo": NSNull()]
+    ]]
 
+    }
+
+extension MockNetworkService{
+            enum responseWithError: Error{
+                case responseError
+            }
+    
+    func fetchDataFromJson(compiltionHandler: @escaping ([Leagues]?, Error?) -> Void){
+                do{
+                    let data = try JSONSerialization.data(withJSONObject: fakeJSONObj)
+                    let  response  = try JSONDecoder().decode(LeaguesResponse.self, from: data)
+                    result = response.result
+                    print ("resulr after response \(result)")
+                    
+                }catch{
+                    print(error.localizedDescription)
+                }
+                if shouldReturnError{
+                    compiltionHandler(nil, responseWithError.responseError)
+                    
+                }else{
+                    compiltionHandler(result, nil)
+                }               
+       }
 }
-
